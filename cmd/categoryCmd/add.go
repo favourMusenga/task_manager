97639ac752b59620d1f277f88b94a509ce701911
Favour Musenga <favourmusenga@gmail.com>
2026-05-19 23:10:50 +0200

    add the ability to add categories using cli

--- cmd/categoryCmd/add.go
+++ cmd/categoryCmd/add.go
@@ -1,8 +1,11 @@
 package categorycmd
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/favourmusenga/task-manager/internals/categories"
+	"github.com/favourmusenga/task-manager/internals/db"
 	"github.com/spf13/cobra"
 )
 
@@ -10,8 +13,41 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a category",
 	Long:  "Create a new task category.",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("Adding a new category...")
+
+		categoryName := args[0]
+
+		db, err := db.GetDb("./task_manager.db")
+
+		if err != nil {
+			return fmt.Errorf("Database connection error: %w", err)
+		}
+
+		ctx := context.Background()
+
+		categoryOptions := categories.CategoriesOptions{}
+
+		categoryDescription, err := cmd.Flags().GetString("description")
+
+		if err != nil {
+			return err
+		}
+
+		if categoryDescription != "" {
+			categoryOptions.Description = &categoryDescription
+		}
+
+		err = categories.AddCategory(db, ctx, categoryName, categoryOptions)
+
+		if err != nil {
+			return fmt.Errorf("Failed to add categories %w", err)
+		}
+
+		fmt.Printf("Added category to database")
+
+		return nil
+
 	},
 	Args: cobra.ExactArgs(1),
 }
@@ -20,4 +56,5 @@ func init() {
 	categoryCmd.AddCommand(addCmd)
 
 	// Here you will define your flags and configuration settings.
+	addCmd.Flags().StringP("description", "d", "", "Add categoory description")
 }
--- internals/categories/category.go
+++ internals/categories/category.go
@@ -1,1 +1,18 @@
 package categories
+
+import (
+	"context"
+
+	dbm "github.com/favourmusenga/task-manager/internals/db"
+	"gorm.io/gorm"
+)
+
+type CategoriesOptions struct {
+	Description *string
+}
+
+func AddCategory(dbc *gorm.DB, ctx context.Context, name string, categoryOption CategoriesOptions) error {
+	err := gorm.G[dbm.Category](dbc).Create(ctx, &dbm.Category{Name: name})
+
+	return err
+}
