94435ecbc0de18ffe92e9c2a596a1765686069a5
Favour Musenga <favourmusenga@gmail.com>
2026-05-25 17:09:00 +0200

    added flags to the add task command

--- cmd/todoCmd/add.go
+++ cmd/todoCmd/add.go
@@ -3,6 +3,8 @@ package todocmd
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/favourmusenga/task-manager/internals/db"
 	"github.com/favourmusenga/task-manager/internals/todos"
@@ -25,6 +27,58 @@ var addCmd = &cobra.Command{
 			return fmt.Errorf("Database connection error: %w", err)
 		}
 
+		priority, err := cmd.Flags().GetString("priority")
+
+		if err != nil {
+			return err
+		}
+
+		profile, err := cmd.Flags().GetString("profile")
+
+		if err != nil {
+			return err
+		}
+
+		categoryId, err := cmd.Flags().GetUint("category-id")
+
+		if err != nil {
+			return err
+		}
+
+		dueDate, err := cmd.Flags().GetString("due-date")
+
+		if err != nil {
+			return err
+		}
+
+		priority = strings.ToLower(priority)
+		profile = strings.ToLower(profile)
+
+		if priority != "low" && priority != "medium" && priority != "high" {
+			return fmt.Errorf("Priority must be 'low', 'medium' or 'high'")
+		}
+
+		if profile != "work" && profile != "personal" {
+			return fmt.Errorf("Profile must be 'work' or 'personal'")
+		}
+
+		if categoryId > 0 {
+			todoOption.CategoryId = categoryId
+		}
+
+		if dueDate != "" {
+			t, err := time.Parse("2006-01-02", dueDate)
+
+			if err != nil {
+				return nil
+			}
+
+			todoOption.DueDate = t
+		}
+
+		todoOption.Priority = db.Priority(priority)
+		todoOption.Profile = db.Profile(profile)
+
 		ctx := context.Background()
 
 		err = todos.AddTodo(dbc, ctx, title, todoOption)
@@ -44,4 +98,9 @@ func init() {
 	todoCmd.AddCommand(addCmd)
 
 	// Here you will define your flags and configuration settings.
+	addCmd.Flags().UintP("category-id", "c", 0, "Add Category to task")
+	addCmd.Flags().StringP("due-date", "d", "", "Add due date in YYYY-MM-DD format like 2026-02-01")
+	addCmd.Flags().StringP("priority", "p", "low", "Add Priority")
+	addCmd.Flags().String("profile", "personal", "Choose profile")
+
 }
--- internals/todos/todos.go
+++ internals/todos/todos.go
@@ -2,21 +2,33 @@ package todos
 
 import (
 	"context"
+	"time"
 
 	dbm "github.com/favourmusenga/task-manager/internals/db"
 	"gorm.io/gorm"
 )
 
 type TodosOptions struct {
-	dueDate     string
-	priotriy    dbm.Priority
-	profile     string
-	description string
-	categories  string
+	DueDate     time.Time
+	Priority    dbm.Priority
+	Profile     dbm.Profile
+	Description string
+	CategoryId  uint
 }
 
 func AddTodo(dbc *gorm.DB, ctx context.Context, title string, todoOptions TodosOptions) error {
-	err := gorm.G[dbm.Todo](dbc).Create(ctx, &dbm.Todo{Title: title})
+	var categoryId *uint
+	var dueDate *time.Time
+
+	if todoOptions.CategoryId > 0 {
+		categoryId = &todoOptions.CategoryId
+	}
+
+	if !todoOptions.DueDate.IsZero() {
+		dueDate = &todoOptions.DueDate
+	}
+
+	err := gorm.G[dbm.Todo](dbc).Create(ctx, &dbm.Todo{Title: title, Priority: todoOptions.Priority, Profile: todoOptions.Profile, DueDate: dueDate, CategoryID: categoryId})
 
 	return err
 }
